9031d574a51999b03593b628c5284b1b2ce08475
hessef <35468516+hessef@users.noreply.github.com>
2025-11-19 12:08:24 -0600

    fixed mappings

---
+++ src/client/java/bluestone/bluestone/BluestoneDataGenerator.java
@@ -0,0 +1,11 @@
+package bluestone.bluestone;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BluestoneDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
